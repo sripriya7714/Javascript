f09555779609c10b060352afcb11becf93806c6b
sripriya7714 <104065618+sripriya7714@users.noreply.github.com>
2022-04-20 14:52:21 +0530

    Add files via upload

---
+++ externalwholenumber.js
@@ -0,0 +1,26 @@
+<html>
+<head>
+<meta charset="ISO-8859-1">
+
+    <title>Insert title here</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<script>
+
+document.write("wholenumber.html");
+</script>
+</head>
+<body style="background-color:orange">
+<h2>WholeNumber</h2>
+<form name="f1">
+<input type="button" value="wholeNumber" onclick="WholeNumber()"/>
+</form>
+</body>
+</html>
---
+++ wholenumber.js
@@ -0,0 +1,16 @@
+
+function WholeNumber(){
+var num1=prompt("enter first number");
+if(num1>=0)
+{
+document.write("<h2>");
+document.writeln(num1 +"is wholenumber");
+document.write("</h2>");
+}
+else
+{
+document.write("<h2>");
+document.writeln(num1 +"is not a wholenumber");
+document.write("</h2>");
+}
+}
